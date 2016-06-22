10ce1e994f482967641e6660324fe900a109a0f3
WenpingWang <wenping0820@163.com>
2016-06-23 00:34:41 +0800

    游戏过程制作
    1、响应滑动事件；
    2、消除控制；
    3、消除后移动、生成新的动物控制；

---
+++ Assets/Scripts/DebugUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DebugUtils {
+	// 将文字更新到Text控件
+	public static void textToUI(GameObject obj, string text){
+		if (obj == null || text == null) {
+			return;
+		}
+		obj.GetComponent<Text> ().text = text;
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	bool isDebug = true;
+	bool testMode = false;
 	public GameObject idleBear, idleCat, idleChicken, idleFox, idleFrog, idleHorse;
 	public GameObject gameBoard;
 	public GameObject gameGrid;
 	public GameObject tileSelect;
+	public GameObject destroyEffect;
+	public GameObject debugText;
 
 	GameObject[] idleAnimals;
 	string[] clickAnimParam;
 	GameObject tileSelectInstance;
+	GameObject selectAnimalObj = null;
 
 	int[,] animalIndexArray = new int[7, 7]{
 		{-1, 0, 0, 0, -1, -1, -1},
@@ -72,46 +76,75 @@ public class GameController : MonoBehaviour {
 
 	void initGameBoard(){
 		initGameGrid ();
-		createAnimals ();
+		if (testMode) {
+			createTestAnimals ();
+		} else {
+			createAnimals ();
+		}
+		updateDebugToUI ();
 	}
 
 	void initGameGrid(){
 		// TODO 动态隐藏方格
 	}
+	/// <测试代码>
+	void createTestAnimals () {
+		animalIndexArray = new int[7, 7]{
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1, -1, -1}};
+
+		createAnimalAtPosition (1, 0, 1);
+		createAnimalAtPosition (1, 1, 1);
+		createAnimalAtPosition (1, 2, 2);
+		createAnimalAtPosition (2, 2, 1);
+	}
+	/// </测试代码>
 
 	void dealWithTouchEvent (){
+		Vector2 upPos = Input.mousePosition;
+
 		if (Input.GetMouseButtonDown (0)) {
 			mouseDownPos = Input.mousePosition;
+			// 点击事件
+			dealWithClick (upPos.x, upPos.y);
 		} else if (Input.GetMouseButtonUp(0)) {
-			Vector2 upPos = Input.mousePosition;
 			float moveX = upPos.x - mouseDownPos.x;
 			float moveY = upPos.y - mouseDownPos.y;
 			if (Mathf.Abs (moveX) > 10 || Mathf.Abs (moveY) > 10) {
 				dealWithMove (moveX, moveY);
-			} else {
-				dealWithClick (upPos.x, upPos.y);
 			}
 		}
 	}
 
+	// 处理滑动事件
 	void dealWithMove(float moveX, float moveY){
-		if (isDebug) Debug.Log ("========= Move START===================================");
+		//if (isDebug) Debug.Log ("========= Move START===================================");
 		if (Mathf.Abs (moveX) > Mathf.Abs (moveY)) {
 			if (moveX > 0) {
 				if(isDebug) Debug.Log ("Right");
+				switchTwoBlocks (2);
 			} else {
 				if(isDebug) Debug.Log ("Left");
+				switchTwoBlocks (0);
 			}
 		} else {
 			if (moveY > 0) {
 				if(isDebug) Debug.Log ("UP");
+				switchTwoBlocks (1);
 			} else {
 				if(isDebug) Debug.Log ("Down");
+				switchTwoBlocks (3);
 			}
 		}
-		if (isDebug) Debug.Log ("=========Move END===================================");
+		//if (isDebug) Debug.Log ("=========Move END===================================");
 	}
 
+	// 处理点击事件
 	void dealWithClick(float xPos, float yPos){
 		Vector2 clickPos = posToIndex (xPos, yPos);
 		int x = (int)clickPos.x;
@@ -123,11 +156,14 @@ public class GameController : MonoBehaviour {
 			return;
 		}
 
-		toggleClickAnim ((int)lastClickAnimPos.x, (int)lastClickAnimPos.y, false);
-		toggleClickAnim (x, y, true);
+		toggleClickAnim (lastClickAnimPos, false);
+		toggleClickAnim (clickPos, true);
 	}
 
-	void toggleClickAnim(int x, int y, bool play){
+	// 播放 / 停止点击动画
+	void toggleClickAnim(Vector2 pos, bool play){
+		int x = (int)pos.x;
+		int y = (int)pos.y;
 		int animIndex = animalIndexArray [x, y];
 		if (animIndex > 0) {
 			Animator anim = animalObjArray[x, y].GetComponent<Animator>();
@@ -135,9 +171,14 @@ public class GameController : MonoBehaviour {
 				anim.SetBool (clickAnimParam[animIndex - 1], play);
 				if (play) {
 					lastClickAnimPos = new Vector2 (x, y);
+					selectAnimalObj = animalObjArray [x, y];
 
 					// 边框动画
 					createTileSelectAtPos (x, y);
+				} else {
+					if (tileSelectInstance != null) {
+						DestroyObject (tileSelectInstance);
+					}
 				}
 			}
 		}
@@ -145,21 +186,14 @@ public class GameController : MonoBehaviour {
 
 	// 创建选中边框动画
 	void createTileSelectAtPos(int x, int y){
-		if (tileSelectInstance != null) {
-			DestroyObject (tileSelectInstance);
-		}
-
-		Vector2 pos = getBlockPos (x, y);
-
-		tileSelectInstance = (GameObject)Instantiate (tileSelect, 
-			new Vector3(pos.x, pos.y, 0), Quaternion.identity); 
-		tileSelectInstance.transform.SetParent(gameBoard.transform, false);
+		tileSelectInstance = createGameObjAtPos (tileSelect, x, y);
 
 		Animator anim = tileSelectInstance.GetComponent<Animator> ();
 		if (anim != null) {
 			anim.SetBool ("tile_select_start", true);
 		}
 	}
+
 	// 世界坐标与位置的对应
 	Vector2 posToIndex(float xPos, float yPos){
 		float x = xPos - block0StartInWorld.x;
@@ -175,12 +209,39 @@ public class GameController : MonoBehaviour {
 		for (int i = 0; i < xNum; i++) {
 			for (int j = 0; j < yNum; j++) {
 				if (animalIndexArray [i, j] != -1) {
-					int index = Random.Range (1, idleAnimals.Length);
+					int index = randomNewAnimalIndexWhenInit(i, j);
 					createAnimalAtPosition (i, j, index);
 					animalIndexArray [i, j] = index;
 				}
 			}
 		}
+	}
+
+	// 初始化游戏时随机生成一个小动物
+	int randomNewAnimalIndexWhenInit(int x, int y){
+		int index = Random.Range (1, idleAnimals.Length);
+		int[,] arr = animalIndexArray;
+
+		if (x < 2 && y < 2) {
+			// do nothing
+		} else if (y < 2) {
+			// 只做竖向检查
+			while (index == arr [x - 1, y] && index == arr [x - 2, y]) {
+				index = Random.Range (1, idleAnimals.Length);
+			}
+		} else if (x < 2) {
+			// 只做横向检查
+			while (index == arr [x, y - 1] && index == arr [x, y - 2]) {
+				index = Random.Range (1, idleAnimals.Length);
+			}
+		} else {
+			// 横向、竖向都检查
+			while ((index == arr [x - 1, y] && index == arr [x - 2, y]) ||
+				(index == arr [x, y - 1] && index == arr [x, y - 2])) {
+				index = Random.Range (1, idleAnimals.Length);
+			}
+		}
+		return index;
 	}
 
 	// 创建一个小动物
@@ -193,10 +254,8 @@ public class GameController : MonoBehaviour {
 			Debug.Log ("Error!!! position out of bound");
 			return;
 		}
-		Vector2 pos = getBlockPos (x, y);
-		GameObject child = (GameObject)Instantiate (idleAnimals[animalIndex - 1], 
-			new Vector3(pos.x, pos.y, 0), Quaternion.identity); 
-		child.transform.SetParent(gameBoard.transform, false);
+
+		GameObject child = createGameObjAtPos (idleAnimals[animalIndex - 1], x, y);
 		animalObjArray [x, y] = child;
 		animalIndexArray [x, y] = animalIndex;
 	}
@@ -207,5 +266,261 @@ public class GameController : MonoBehaviour {
 		float yPos = block0Center.y - y * (blockSize + blockGap);
 
 		return new Vector2 (xPos, yPos);
+	}
+
+	// 获得某个block的世界坐标位置
+	Vector2 getBlockWorldPos(int x, int y) {
+		float xPos = (block0Center.x + x * (blockSize + blockGap)) * screenRadio;
+		float yPos = (block0Center.y - y * (blockSize + blockGap)) * screenRadio;
+		return new Vector2 (xPos + gameBoardCenter.x, yPos + gameBoardCenter.y);
+	}
+
+	// 交换两个方块（选中方块与其上 / 下 / 左 / 右的方块）
+	// 左：0，上：1，右：2，下：3
+	// 问题：需要在交换动画完成时再去消除，否则消除过程不太流畅
+	void switchTwoBlocks(int orientation){
+		if (null == selectAnimalObj){
+			if (isDebug) Debug.Log ("switchTwoBlocks: selectAnimalObj is null");
+			return;
+		}
+		Vector2 start = lastClickAnimPos;
+		Vector2 end = getSwitchEndPos (start, orientation);
+
+		if (canMoveTo (end)) {
+			// 交换位置
+			switchPos(start, end, true);
+			// 停止动画
+			if (lastClickAnimPos != null) {
+				toggleClickAnim (lastClickAnimPos, false);
+			}
+		} else {
+			if (isDebug) Debug.Log ("switchTwoBlocks: no need to switch");
+		}
+	}
+
+	// 交换位置
+	void switchPos(Vector2 start, Vector2 end, bool clearWhenSwitchFinish){
+		Vector2 startPos = getBlockWorldPos ((int)start.x, (int)start.y);
+		Vector2 endPos = getBlockWorldPos ((int)end.x, (int)end.y);
+
+		if (animalObjArray [(int)start.x, (int)start.y] == null) {
+			if (isDebug) Debug.Log ("switchPos animalObjArray [" + (int)start.x + ", " +  (int)start.y + "] is null");
+			return;
+		}
+		if (animalObjArray [(int)end.x, (int)end.y] == null) {
+			if (isDebug) Debug.Log ("switchPos animalObjArray [" + (int)end.x + ", " +  (int)end.y + "] is null");
+			return;
+		}
+
+		if (clearWhenSwitchFinish) {
+			// 移动完调用switchPosFinish方法；
+			iTween.MoveTo (animalObjArray [(int)start.x, (int)start.y],
+				iTween.Hash ("x", endPos.x, "y", endPos.y, "delay", .2,
+					"oncomplete", "switchPosFinish", 
+					"oncompleteparams", new Vector2[]{end, start},
+					"oncompletetarget", gameObject));
+		} else {
+			iTween.MoveTo (animalObjArray [(int)start.x, (int)start.y],
+				iTween.Hash ("x", endPos.x, "y", endPos.y, "delay", .2));
+		}
+		iTween.MoveTo (animalObjArray [(int)end.x, (int)end.y],
+			iTween.Hash ("y", startPos.y, "x", startPos.x, "delay", .2));
+		// 交换坐标
+		int record = animalIndexArray [(int)start.x, (int)start.y];
+		animalIndexArray [(int)start.x, (int)start.y] = animalIndexArray [(int)end.x, (int)end.y];
+		animalIndexArray [(int)end.x, (int)end.y] = record;
+
+		GameObject recordObj = animalObjArray [(int)start.x, (int)start.y];
+		animalObjArray [(int)start.x, (int)start.y] = animalObjArray [(int)end.x, (int)end.y];
+		animalObjArray [(int)end.x, (int)end.y] = recordObj;
+	}
+
+	// 交换动物结束时判断消除
+	void switchPosFinish(Vector2[] positions){
+		if (null == positions || positions.Length < 2) {
+			return;
+		}
+		// 判断能否消除
+		bool isCanClear = canClear();
+		// 不能消除，位置换回来
+		if (!isCanClear) {
+			switchPos (positions[0], positions[1], false);
+
+			updateDebugToUI ();
+		}
+	}
+
+	Vector2 getSwitchEndPos(Vector2 switchStart, int orientation){
+		if (0 == orientation) {
+			// 向左
+			return new Vector2 (switchStart.x - 1, switchStart.y);
+		} else if (1 == orientation) {
+			// 向上
+			return new Vector2 (switchStart.x, switchStart.y - 1);
+		} else if (2 == orientation) {
+			// 向右
+			return new Vector2 (switchStart.x + 1, switchStart.y);
+		} else if (3 == orientation) {
+			// 向下
+			return new Vector2 (switchStart.x, switchStart.y + 1);
+		}
+		return Vector2.zero;
+	}
+
+	bool canMoveTo(Vector2 pos){
+		return pos != Vector2.zero && pos.x >= 0 && pos.y >= 0 &&
+			pos.x < xNum && pos.y < yNum &&
+			animalIndexArray [(int)pos.x, (int)pos.y] != -1;
+	}
+
+	// 判断能否消除
+	bool canClear(){
+		bool canClear = false;
+		for (int i = 0; i < xNum; i++) {
+			for (int j = 0; j < yNum; j++) {
+				if (animalIndexArray [i, j] != -1) {
+					if (i < xNum - 2 && animalIndexArray [i, j] == animalIndexArray [i + 1, j] &&
+					   animalIndexArray [i, j] == animalIndexArray [i + 2, j]) {
+						// 横向消除
+						clearAnimals(new Vector2[]{new Vector2(i, j),
+							new Vector2(i + 1, j), new Vector2(i + 2, j)});
+						canClear = true;
+					}
+					if (j < yNum - 2 && animalIndexArray [i, j] == animalIndexArray [i, j + 1] &&
+						animalIndexArray [i, j] == animalIndexArray [i, j + 2]) {
+						// 竖向消除
+						clearAnimals(new Vector2[]{new Vector2(i, j),
+							new Vector2(i, j + 1), new Vector2(i, j + 2)});
+						canClear = true;
+					}
+				}
+			}
+		}
+		return canClear;
+	}
+
+	// 消除动物
+	void clearAnimals(Vector2[] positions){
+		if (positions != null && positions.Length > 0) {
+			foreach(Vector2 vect in positions){
+				GameObject obj = animalObjArray[(int)vect.x, (int)vect.y];
+				if (obj != null) {
+					// Play destory animation
+					createGameObjAtPos (destroyEffect, (int)vect.x, (int)vect.y);
+					Destroy (obj);
+					animalIndexArray [(int)vect.x, (int)vect.y] = 0;
+				}
+			}
+
+			fallDown (new int[]{0, 1, 2, 3, 4, 5, 6});
+		}
+	}
+
+	// 在x,y位置创建游戏对象
+	GameObject createGameObjAtPos(GameObject obj, int x, int y){
+		Vector2 pos = getBlockPos (x, y);
+		GameObject child = (GameObject)Instantiate (obj, 
+			new Vector3(pos.x, pos.y, 0), Quaternion.identity); 
+		child.transform.SetParent(gameBoard.transform, false);
+		return child;
+	}
+
+	// 消除后向下移动
+	void fallDown(int[] cols){
+		if (cols.Length > 0) {
+			foreach(int col in cols){
+				oneColFallDown (col);
+			}
+		}
+	}
+
+	// 某一行向下移动
+	void oneColFallDown(int col){
+		if (isDebug) Debug.Log ("fall down col " + col);
+		bool fallDownFinishCalled = false;
+
+		for (int i = yNum - 2; i >= 0; i--) {
+			if (animalIndexArray [col, i] != -1 && animalIndexArray [col, i] != 0) {
+				for (int j = yNum - 1; j > i; j--) {
+					if (0 == animalIndexArray [col, j]) {
+						animalIndexArray [col, j] = animalIndexArray [col, i];
+						animalIndexArray [col, i] = 0;
+						animalObjArray [col, j] = animalObjArray [col, i];
+						animalObjArray [col, i] = null;
+
+						Vector2 endPos = getBlockWorldPos (col, j);
+						if (animalObjArray [col, j] == null) {
+							if (isDebug) Debug.Log ("oneColFallDown animalObjArray [" + col + ", " +  j + "] is null");
+							return;
+						}
+
+						if (fallDownFinishCalled) {
+							iTween.MoveTo (animalObjArray [col, j],
+								iTween.Hash ("y", endPos.y, "x", endPos.x, "delay", .2));
+						} else {
+							fallDownFinishCalled = true;
+							iTween.MoveTo (animalObjArray [col, j],
+								iTween.Hash ("y", endPos.y, "x", endPos.x, "delay", .2,
+									"oncomplete", "fallDownFinish", 
+									"oncompleteparams", col,
+									"oncompletetarget", gameObject));
+						}
+						break;
+					}
+				}
+			}
+		}
+		if (!fallDownFinishCalled) {
+			fallDownFinish (col);
+		}
+	}
+
+	void fallDownFinish(int col){
+		for (int i = yNum - 1; i >= 0; i--) {
+			if (0 == animalIndexArray [col, i]) {
+				// 生成新的动物并从顶部移动到col，i的位置
+				createNewAndFallDown(col, i);
+			}
+		}
+
+		updateDebugToUI ();
+	}
+
+	void createNewAndFallDown(int x, int y){
+		int index = getRandomWhenClear ();
+		if (isDebug) Debug.Log ("createNewAndFallDown at[" + x + ", " + y + "] animal=" + idleAnimals[index - 1].name);
+		GameObject child = createGameObjAtPos (idleAnimals[index - 1], x, 0);
+		animalObjArray [x, y] = child;
+		animalIndexArray [x, y] = index;
+
+		Vector2 pos = getBlockWorldPos (x, y);
+		iTween.MoveTo (child,
+			iTween.Hash ("y", pos.y, "x", pos.x, "delay", .2));
+	}
+
+	int randomRecord = -1;
+	int getRandomWhenClear(){
+		if (testMode) {
+			randomRecord++;
+			return randomRecord % 3 + 1;
+		} else {
+			return Random.Range (1, idleAnimals.Length);
+		}
+	}
+
+	string getAnimalIndexArrayString(int[,] array) {
+		string text = "";
+		for (int i = 0; i < yNum; i++) {
+			for (int j = 0; j < xNum; j++) {
+				text += (array[j, i] == -1 ? "      " : (array[j, i]) + ",   ");
+			}
+			text += "\n";
+		}
+		return text;
+	}
+
+	void updateDebugToUI(){
+		string text = getAnimalIndexArrayString (animalIndexArray);
+		DebugUtils.textToUI (debugText, text);
 	}
 }
